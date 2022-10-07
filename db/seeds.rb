60a1bffbec20a101c23db98ff795fee4441c6157
Juan Camilo Ramirez Velez <juancrv28@gmail.com>
2022-10-07 13:36:53 -0500

    merged changes from home update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning database...'
+Service.destroy_all
+Pet.destroy_all
+User.destroy_all
+
+10.times do
+  user = User.new
+  if rand < 0.5
+    user.name = Faker::Name.female_first_name
+  else
+    user.name = Faker::Name.male_first_name
+  end
+  user.email = Faker::Internet.email(name: user.name)
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.address = Faker::Address.street_address
+  user.phone_number = Faker::PhoneNumber.cell_phone_in_e164
+  user.region = REGIONS.sample
+  user.city = COLOMBIA[user.region].sample
+  user.petwalker = rand < 0.5
+
+  if user.petwalker
+    user.description = Faker::Lorem.paragraph
+    user.price_per_hour = rand(5000..8000)
+  else
+    pet = Pet.new
+    pet.user = user
+    pet.name = 'My pet'
+    pet.save!
+  end
+
+  user.save!
+  if user.petwalker
+    puts "User #{user.name} created!"
+  else
+    puts "User #{user.name} created! Pet => #{pet.name}"
+  end
+end
